b94625d3a6ebc623400447bf5adf0c060c4b4b16
GamGGyul <71317753+moony564@users.noreply.github.com>
2022-12-25 17:03:15 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.py
@@ -0,0 +1,12 @@
+def on_forever():
+    if pins.digital_read_pin(DigitalPin.P1) < 0:
+        pass
+    elif pins.digital_read_pin(DigitalPin.P1) < 0:
+        pass
+    elif pins.digital_read_pin(DigitalPin.P1) < 0:
+        pass
+    elif pins.digital_read_pin(DigitalPin.P1) < 0:
+        pass
+    elif pins.digital_read_pin(DigitalPin.P1) < 0:
+        pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P1) < 0) {
+    	
+    } else if (pins.digitalReadPin(DigitalPin.P1) < 0) {
+    	
+    } else if (pins.digitalReadPin(DigitalPin.P1) < 0) {
+    	
+    } else if (pins.digitalReadPin(DigitalPin.P1) < 0) {
+    	
+    } else if (pins.digitalReadPin(DigitalPin.P1) < 0) {
+    	
+    }
+})
